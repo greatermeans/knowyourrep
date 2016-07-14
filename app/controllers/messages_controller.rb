e7b8c3d6cdb86a6bf616ff37ce9694c69c0dc8d2
Dan Gorman <danielhgorman@gmail.com>
2016-07-14 17:24:23 -0400

    added messages to politician show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,14 @@
 class MessagesController < ApplicationController
+	def new
+		@message = Message.new
+	end
+
+	def create
+		@message = Message.new
+		@message.subject = params[:message][:subject]
+		@message.content = params[:message][:content]
+		@message.user_id = session[:user_id]
+		@message.politician_id = request.referer[34..-1].to_i
+		binding.pry
+	end
 end
